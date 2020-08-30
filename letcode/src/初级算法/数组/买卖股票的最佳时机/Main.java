fac11be6974971db523bc68a140c57365b90b854
chocho <1961147030@qq.com>
2020-08-30 15:25:56 +0800

    2,股票补充

--- letcode/src/初级算法/数组/买卖股票的最佳时机/Main.java
+++ letcode/src/初级算法/数组/买卖股票的最佳时机/Main.java
@@ -1,32 +1,39 @@
 package 初级算法.数组.买卖股票的最佳时机;
 
+/**
+ * int maxProfit(int[] prices);
+ *
+ * 题目分析：
+ * 买股票获得最大利润，即在谷买入，在峰卖出，中间商赚差价。因此使用峰谷法即可实现。
+ * 题目需要实现找到峰和谷
+ * （1）找到峰
+ * 遍历数组，若下一个价格比当前大，则继续；否则，则找到峰。
+ * （2）找到谷
+ * 遍历数组，若下一个价格比当前小，则继续；否则，则找到谷。
+ */
 public class Main {
     public static void main(String[] args) {
 
     }
 
     public int maxProfit(int[] prices) {
-        int low = 0;    //高价标志
-        int hight = 0;  //低价标志
-        int status = 0; //0-卖出状态,1-买入状态
-        int count = 0;
+        int low = 0;    //谷标志
+        int high = 0;  //峰标志
+        int count = 0;  //利润
+        int i = 0;
 
-        //在未来最低的价格买入，在未来最高的价格卖出
-        //求较大数，较小数
-        for(int i = 1; i < prices.length; i++){
-            //找小数
-            if(status == 0){
-                if(prices[i] < prices[hight]){
-                    low = i;
-                    status = 1;
-                }
-            }else{
-                if(prices[i] > prices[low]){
-                    hight = i;
-                    count += (prices[hight]-prices[low]);
-                    status = 0;
-                }
+        //找峰找谷
+        while(i < prices.length-1){
+            while(i < prices.length-1 && prices[i] >= prices[i+1]){
+                i++;
             }
+            low = i;
+            while(i < prices.length-1 && prices[i] <= prices[i+1]){
+                i++;
+            }
+            high = i;
+            //计算利润
+            count += (prices[high] - prices[low]);
         }
 
         return count;
